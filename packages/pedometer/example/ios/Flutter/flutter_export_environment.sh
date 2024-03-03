da1de1795b60abde72288701bcb9893d3dc87766
Sho Morita <sho.morita@clementec.com>
2024-03-03 21:54:53 +0900

    環境差分変更

--- packages/pedometer/example/ios/Flutter/flutter_export_environment.sh
+++ packages/pedometer/example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/bardram/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/bardram/dev/flutter-plugins/packages/pedometer/example"
+export "FLUTTER_ROOT=C:\Program Files\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\year_\Documents\workspace\flutter-plugins\packages\pedometer\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
